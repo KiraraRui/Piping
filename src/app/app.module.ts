1c1de6254018b1dbf10e0eedf9107016a55b14ae
kira achen <kira.kira@live.dk>
2020-08-11 23:56:06 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PipesPipe } from './pipes.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PipesPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/morse.spec.ts
@@ -0,0 +1,7 @@
+import { Morse } from './morse';
+
+describe('Morse', () => {
+  it('should create an instance', () => {
+    expect(new Morse()).toBeTruthy();
+  });
+});
